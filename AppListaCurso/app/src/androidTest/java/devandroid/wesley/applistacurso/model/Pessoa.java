85372e9adbf1a6d647c0da755d48b61e5fe9b6ee
Wesley <yinnihisoka@gmail.com>
2024-01-20 12:33:28 -0300

    criando classe pessoa

---
+++ AppListaCurso/app/src/androidTest/java/devandroid/wesley/applistacurso/model/Pessoa.java
@@ -0,0 +1,41 @@
+package devandroid.wesley.applistacurso.model;
+
+public class Pessoa {
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    private String telefoneContato;
+}
