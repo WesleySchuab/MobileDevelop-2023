edeaffb72da8d843589bc8c4d947693cdeecee60
Wesley <yinnihisoka@gmail.com>
2024-01-20 18:22:03 -0300

    usando metodo get

--- AppListaCurso/app/src/main/java/devandroid/wesley/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/wesley/applistacurso/view/MainActivity.java
@@ -3,12 +3,14 @@ package devandroid.wesley.applistacurso.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import devandroid.wesley.applistacurso.R;
 import devandroid.wesley.applistacurso.view.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
+    String dadosPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setSobreNome("Jatim ");
         pessoa.setCursoDesejado("Alquimia");
         pessoa.setTelefoneContato("1499911911");
+
+        dadosPessoa = "Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa = " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+
     }
 
 
