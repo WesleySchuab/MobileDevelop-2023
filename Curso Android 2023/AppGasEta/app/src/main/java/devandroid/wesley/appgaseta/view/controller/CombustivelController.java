ab88647886f836680cb168f62aeb8448cbad277b
Wesley <yinnihisoka@gmail.com>
2024-01-26 19:36:21 -0300

    criando string para o nome do arquivo

--- Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
+++ Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
@@ -5,4 +5,6 @@ import android.content.SharedPreferences;
 public class CombustivelController {
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor listaVip;
+    public static final String  NOME_PRERENCES ="pref_gaseta";
+
 }
