c1aa4dd0a630c553f650687290a9ab26b35c1c3e
Wesley <yinnihisoka@gmail.com>
2024-01-26 19:52:25 -0300

    criando a a classe CombustivelController

--- Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
+++ Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
@@ -1,10 +1,26 @@
 package devandroid.wesley.appgaseta.view.controller;
 
+import static devandroid.wesley.appgaseta.view.controller.PessoaController.NOME_PREFERENCES;
+
 import android.content.SharedPreferences;
+
+import devandroid.wesley.appgaseta.view.MainActivity;
+import devandroid.wesley.appgaseta.view.model.Combustivel;
 
 public class CombustivelController {
     SharedPreferences sharedPreferences;
-    SharedPreferences.Editor listaVip;
-    public static final String  NOME_PRERENCES ="pref_gaseta";
+    SharedPreferences.Editor dadosPreferences;
+    public static final String NOME_PRERENCES = "pref_gaseta";
 
+    public CombustivelController(MainActivity mainActivity) {
+        sharedPreferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        dadosPreferences = sharedPreferences.edit();
+    }
+    public void salvar(Combustivel combustivel){
+        dadosPreferences.putString("combustivel",combustivel.getNomeDoCombustivel());
+        dadosPreferences.putFloat("precoDoCombustivel",(float) combustivel.getPrecoDoCombustivel());
+        dadosPreferences.putString("recomendacao",combustivel.getRecomendacao());
+        dadosPreferences.apply();
+
+    }
 }
