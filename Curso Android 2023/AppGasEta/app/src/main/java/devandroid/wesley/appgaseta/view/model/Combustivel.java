2427c3b8f55cc8db963c9f6c02a6eb890c6d3af7
Wesley <yinnihisoka@gmail.com>
2024-01-26 10:47:58 -0300

    criando classe Combustivel

---
+++ Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/model/Combustivel.java
@@ -0,0 +1,30 @@
+package devandroid.wesley.appgaseta.view.model;
+
+public class Combustivel {
+    private String nomeDoCombustivel;
+    private double precoDoCombustivel;
+    private String recomendacao;
+    public String getNomeDoCombustivel() {
+        return nomeDoCombustivel;
+    }
+
+    public void setNomeDoCombustivel(String nomeDoCombustivel) {
+        this.nomeDoCombustivel = nomeDoCombustivel;
+    }
+
+    public double getPrecoDoCombustivel() {
+        return precoDoCombustivel;
+    }
+
+    public void setPrecoDoCombustivel(double precoDoCombustivel) {
+        this.precoDoCombustivel = precoDoCombustivel;
+    }
+
+    public String getRecomendacao() {
+        return recomendacao;
+    }
+
+    public void setRecomendacao(String recomendacao) {
+        this.recomendacao = recomendacao;
+    }
+}
