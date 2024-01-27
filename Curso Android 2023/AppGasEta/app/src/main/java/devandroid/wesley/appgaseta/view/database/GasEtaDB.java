b6be08b07915db699bd8f5368c7ec795bd1af21c
Wesley <yinnihisoka@gmail.com>
2024-01-27 18:05:01 -0300

    criando contrutor do SQL

--- Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/database/GasEtaDB.java
+++ Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/database/GasEtaDB.java
@@ -1,9 +1,16 @@
 package devandroid.wesley.appgaseta.view.database;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class GasEtaDB extends SQLiteOpenHelper {
+    public GasEtaDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
