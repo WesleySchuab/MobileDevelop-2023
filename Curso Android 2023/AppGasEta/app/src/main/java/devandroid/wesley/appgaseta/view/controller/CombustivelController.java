db8034654a4a3b9d896559f365af7be677e98e0d
Wesley <yinnihisoka@gmail.com>
2024-01-28 08:15:00 -0300

    preparando controller para trabalhar com bando de dados

--- Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
+++ Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
@@ -3,14 +3,16 @@ package devandroid.wesley.appgaseta.view.controller;
 
 import android.content.SharedPreferences;
 import devandroid.wesley.appgaseta.view.MainActivity;
+import devandroid.wesley.appgaseta.view.database.GasEtaDB;
 import devandroid.wesley.appgaseta.view.model.Combustivel;
 
-public class CombustivelController {
+public class CombustivelController extends GasEtaDB {
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor dadosPreferences;
     public static final String NOME_PREFERENCES = "pref_gaseta";
 
     public CombustivelController(MainActivity mainActivity) {
+        super(mainActivity);
         sharedPreferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
         dadosPreferences = sharedPreferences.edit();
     }
