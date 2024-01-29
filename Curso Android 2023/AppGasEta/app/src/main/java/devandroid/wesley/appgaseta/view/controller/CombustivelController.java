88083b7c5c27ff364f4b56d783d24781dd6741a0
Wesley <yinnihisoka@gmail.com>
2024-01-29 05:05:53 -0300

    metodo que vai passar as informações a ser alterada

--- Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
+++ Curso Android 2023/AppGasEta/app/src/main/java/devandroid/wesley/appgaseta/view/controller/CombustivelController.java
@@ -37,6 +37,20 @@ public class CombustivelController extends GasEtaDB {
     public List<Combustivel> getListaDeDados(){
         return listarDados();
     }
+    public void alterar(Combustivel combustivel){
+
+        ContentValues dados = new ContentValues();
+
+        dados.put("id",combustivel.getId());
+        dados.put("nomeDoCombustivel", combustivel.getNomeDoCombustivel());
+        dados.put("precoDoCombustivel", combustivel.getPrecoDoCombustivel());
+        dados.put("recomendacao", combustivel.getRecomendacao());
+
+        alterarObjeto("Combustivel",dados);
+
+    }
+
+
     public void limpar(){
         dadosPreferences.clear();
         dadosPreferences.apply();
